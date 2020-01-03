be0e66a6f6afdcb9bae076548b08582766bb968a
Setialife <Setialife@gmail.com>
2020-01-03 19:42:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ 'TelegramProductionURL': 'https://setialife.herokuapp.com/', // do not forget trailing "/" !!
+ 'TelegramToken': '1024179098:AAEQ2kLQDyPIyBwH7UmQZt-TUXAoVUsERpI', // Telegram token
+ 'googleSheetKey': '1WMfy9GTf4_pW8hLS0uVPiLywbXd9T0kyzmO-mRWC-0c', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+}
