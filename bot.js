b2f03601e20574b9438cccf6f7060a7be895f257
Setialife <Setialife@gmail.com>
2020-01-09 06:21:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ bot.onText(/(.+)$/, function (msg, match) {
         }
         
         if (isNaN(targetTime))
-            targetTime = -1;
+            targetTime = parsed;
         
         var formattedAnswer = "";
         
