e5e5dc894dfc099d26288142cd7441be24b56f89
Setialife <Setialife@gmail.com>
2020-01-09 06:53:47 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
-var config = require('./config'); 
+// Created by Evgenii Mironichev, Copyright 2016,
+// based on this awesome tutorial: https://mvalipour.github.io/node.js/2015/11/10/build-telegram-bot-nodejs-heroku/
+
+var config = require('./config'); // rename config.js.example into config.js and set keys and tokens inside it
+
 var Bot = require('node-telegram-bot-api');
 var bot;
 
@@ -10,15 +14,23 @@ else {
   bot = new Bot(config.TelegramToken, { polling: true });
 }
 
+//var Bot = require('node-telegram-bot-api'),
+//    bot = new Bot(config.TelegramToken, { polling: true });
+
 console.log('secon-bot server started...');
 
+// Make sure it is public or set to Anyone with link can view 
+// "od6" is the fist worksheet in the spreadsheet
 var url = "https://spreadsheets.google.com/feeds/list/" + config.googleSheetKey + "/od6/public/values?alt=json";
 
-bot.onText(/(.+)$/, function (msg, match){
+var moment = require('moment-timezone');
 
+bot.onText(/(.+)$/, function (msg, match) {
+    // keywords are anything typed in
   var keywords = match[1];
   var request = require("request");
-
+      
+    // send request to retrieve the spreadsheet as the JSON 
     request(url, function (error, response, body) {
         if (error || response.statusCode != 200) {
             console.log('Error: '+error); // Show the error
@@ -27,9 +39,7 @@ bot.onText(/(.+)$/, function (msg, match){
         }
         
         var parsed = JSON.parse(body);
-
         var targetTime = NaN;
-
         if (!isNaN(keywords))   // isNaN returns false if the value is number
         {
             try{
@@ -44,11 +54,17 @@ bot.onText(/(.+)$/, function (msg, match){
             targetTime = -1;
         
         var formattedAnswer = "";
+        
+        // debug purposes: echo from id: 
+        // formattedAnswer += "\nMsg.from.id=" + msg.from.id + "\n";
+    
+        var currentHours = parseInt(moment().tz(config.confTimeZone).format('HH'),10);
+        var currentMinutes = parseInt(moment().tz(config.confTimeZone).format('mm'),10);
+        // console.log("Current hours: " + currentHours);
         var currentAnswer = "";
-
-
+        
         var itemsFound = 0;
-
+        // sending answers
         parsed.feed.entry.forEach(function(item){
                 // get the time(in hours) from the very first column
                 var itemTime = NaN;
@@ -60,36 +76,43 @@ bot.onText(/(.+)$/, function (msg, match){
                 {
                     itemTime = NaN;
                 }
-
-
+                
                 if (
+                    (!isNaN(itemTime) && itemTime == targetTime) ||
                     (isNaN(itemTime) && itemTitle.toLowerCase().trim() == keywords.toLowerCase().trim())
                     )
                 {
                     // add the line break if not the first answer
                     if (itemsFound==0) 
-                        formattedAnswer += "barang yang anda cari adalah:\n\n";
+                        formattedAnswer += " Barang yang ditanyakan:\n\n";
                     else 
                         formattedAnswer += "\n\n";
                         
                     itemsFound++;
-                    formattedAnswer += '\u27a1' + item.content.$t; 
+                    formattedAnswer += '\u27a1' + item.content.$t; // add item content, '\u27a1' is the arrow emoji
                 }
-		});
-
-
+                else if (currentHours == itemTime) // else collect items for the current hour
+                {
+                    if (currentAnswer == '')
+                        currentAnswer == 'Starting from ' + currentHours + " h the following talks are goinf:\n\n";
+                    else 
+                        currentAnswer += "\n\n"; 
+                        
+                    currentAnswer += '\u27a1' + item.content.$t; // get item content, '\u27a1' is the arrow emoji
+                }
+                
+                // else doing nothing
+        });
+        
+        // if no items were found for the given time 
         if (itemsFound == 0)
         {
-            if (targetTime<0 || targetTime>24)
-                formattedAnswer = "Barang yang anda cari tidak tersedia atau keyword salah.\n\n";
+            if (targetTime<0 || targetTime>24 || currentAnswer != '')
+                formattedAnswer = "Barang yang anda cari tidak tersedia atau keyword salah\n\n";
             else 
-                formattedAnswer = "Can't find events for the given time ";
+                formattedAnswer = "barang yang anda cari tidak ditemukan";
                 
-            // output current answer
-            if (currentAnswer != '')
-            {
-                formattedAnswer += "Paling ngisor";
-            }
+            
         }
     
         // send message telegram finally
@@ -99,13 +122,6 @@ bot.onText(/(.+)$/, function (msg, match){
     
     });
 
-
 });
 
-
-
-
-
-
-
-        
+module.exports = bot;
